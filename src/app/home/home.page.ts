d21e6f95b3b3a2da00e45becb4da283862616e81
Vina del Mar <vina@duoc.acad>
2024-11-05 12:16:32 -0300

    Se creo funcion para cerrar sesion y que se borre el session id del storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController, ToastController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +11,25 @@ import { AlertController, ToastController } from '@ionic/angular';
 
 export class HomePage {
 
-constructor(public mensaje:ToastController, private route:Router, public alerta:AlertController) {}
+constructor(public mensaje:ToastController, private route:Router, public alerta:AlertController, private storage : Storage) {}
 
-ingresar(){
-    this.route.navigate(["/login"])
+async ingresar(){
+
+  await this.storage.create();
+    // Obtener el correo del almacenamiento
+  const SessionId = await this.storage.get("SessionId");
+  
+  if (SessionId) {
+      // Si el correo está almacenado, el usuario ya está autenticado
+      console.log("Usuario autenticado:", SessionId);
+      // Redirigir directamente al inicio
+      this.route.navigate(["/inicio"]);
+  } else {
+      // Si no hay correo almacenado, el usuario no está autenticado
+      this.route.navigate(["/login"]);
+  }
   
 }
-
-
 
 }
 
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit , Input} from '@angular/core';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-menu',
@@ -10,12 +11,26 @@ export class MenuComponent  implements OnInit {
 
   @Input() titulo: string = '';
 
-  constructor(private router : Router) { }
+  constructor(private route : Router, private storage : Storage) { }
 
 
   navigateTo(path: string) {
-    this.router.navigate([path]);
+    this.route.navigate([path]);
   }
   ngOnInit() {}
 
+  cerrarSesion() {
+      // Eliminar la sesión del almacenamiento (Ionic Storage)
+      this.storage.remove("SessionId").then(() => {
+          console.log("Sesión cerrada correctamente.");
+          
+          // Redirigir al usuario a la página de login
+          this.route.navigate(["/home"]);
+      }).catch((error) => {
+          console.error("Error al cerrar sesión", error);
+          // Si ocurre algún error, muestra una alerta o maneja el error
+      });
+  }
 }
+
+
